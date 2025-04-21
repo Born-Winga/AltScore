3dc458f0e38d33d9e4dbbf270e2e62aebf429249
PHILIP OYOO <oyoo@thejumba.com>
2025-04-21 22:38:41 +0300

    chore: clean up

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,11 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import type React from "react";
-import { useAuthStore } from "@/lib/auth/authStore";
-import { redirect } from "next/navigation";
 export default function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	
 	return (
 		<SidebarProvider>
 			<AppSidebar />
